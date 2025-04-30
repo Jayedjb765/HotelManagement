4d5143e4400c6a133363f74d75db9b00556db6e6
Jayedjb765 <jayedjb76@gmail.com>
2025-04-30 18:00:28 +0600

    Adding Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div className="text-5xl">Hello world!</div>,
-  },
-]);
+import router from "./router/router.jsx";
+import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
