1d5047ba5a78fc9eb27a347d41375036acbc8d20
Jayedjb765 <jayedjb76@gmail.com>
2025-04-30 18:50:32 +0600

    Adding Schadule button at navbar

--- src/components/Navbarr/Navbar.jsx
+++ src/components/Navbarr/Navbar.jsx
@@ -1,9 +1,21 @@
-import { Button } from "antd";
+import { CalendarOutlined } from "@ant-design/icons";
+import { Button, Flex, Tooltip } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const Navlinks = (
+    <>
+      <Link className="hover:text-orange-400">Home</Link>
+      <Link className="hover:text-orange-400">Properties</Link>
+      <Link className="hover:text-orange-400">Property Details</Link>
+      <Link className="hover:text-orange-400">Conatct Us</Link>
+    </>
+  );
+
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 ">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -15,64 +27,35 @@ const Navbar = () => {
               stroke="currentColor"
             >
               {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
+              <path strokeLinecap="round" strokeLinejoin="round" />{" "}
             </svg>
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow space-x-4"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {Navlinks}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link className="ml-64 text-3xl  font-extrabold"> VILLA</Link>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+      <div className="navbar-center hidden lg:flex ">
+        <ul className="menu menu-horizontal px-1 space-x-16 font-bold text-xl  ">
+          {Navlinks}
         </ul>
       </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
+      <div className="navbar-end text-xl">
+        <button className="group relative flex items-center bg-black text-white px-6 py-2 rounded-full mr-96 overflow-visible">
+          {/* Orange circle with calendar icon */}
+          <span className="absolute -left-4 w-10 h-10 bg-orange-500 rounded-full flex items-center justify-center">
+            <CalendarOutlined className="text-white text-lg" />
+          </span>
+
+          {/* Text with hover effect */}
+          <span className="ml-6 group-hover:text-orange-400 text-lg font-medium">
+            Schedule a visit
+          </span>
+        </button>
       </div>
     </div>
   );
