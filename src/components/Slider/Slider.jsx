9154f923efd10b5c856b7ad40d7d63509e79e961
Jayedjb765 <jayedjb76@gmail.com>
2025-05-03 21:21:46 +0600

    updating slider for home page

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,58 +3,94 @@ import React from "react";
 const Slider = () => {
   return (
     <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide4" className="btn btn-circle">
+      {/* Slide 1 */}
+      <div id="slide1" className="carousel-item relative h-[900px] w-full">
+        {/* Image with blur */}
+        <div className="absolute inset-0 z-0">
+          <img
+            src="https://i.ibb.co.com/jvbLD2Yk/digital-marketing-agency-ntwrk-g39p1k-Djv-SY-unsplash.jpg"
+            className="w-full h-full object-cover blur-sm"
+          />
+        </div>
+
+        {/* Text Overlay */}
+        <div className="absolute top-1/3 left-20 text-white z-10">
+          <p className="bg-white bg-opacity-80 px-4 py-1 rounded text-black font-medium">
+            Miami,{" "}
+            <span className="text-red-600 font-semibold">South Florida</span>
+          </p>
+          <h1 className="text-5xl font-extrabold mt-4 leading-tight">
+            ACT NOW! <br />
+            GET THE HIGHEST <br />
+            LEVEL PENTHOUSE
+          </h1>
+        </div>
+
+        {/* Arrows */}
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between z-10">
+          <a href="#slide3" className="btn btn-circle ml-20">
             ❮
           </a>
-          <a href="#slide2" className="btn btn-circle">
+          <a href="#slide2" className="btn btn-circle mr-20">
             ❯
           </a>
         </div>
       </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide1" className="btn btn-circle">
+
+      {/* Repeat same structure for slide 2 and slide 3 with blur-sm class */}
+      {/* Slide 2 */}
+      <div id="slide2" className="carousel-item relative h-[900px] w-full">
+        <div className="absolute inset-0 z-0">
+          <img
+            src="https://i.ibb.co.com/bM6tS9Tv/frames-for-your-heart-2d4l-AQAlb-DA-unsplash.jpg"
+            className="w-full h-full object-cover blur-sm"
+          />
+        </div>
+        <div className="absolute top-1/3 left-20 text-white z-10">
+          <p className="bg-white bg-opacity-80 px-4 py-1 rounded text-black font-medium">
+            Miami,{" "}
+            <span className="text-red-600 font-semibold">South Florida</span>
+          </p>
+          <h1 className="text-5xl font-extrabold mt-4 leading-tight">
+            ACT NOW! <br />
+            GET THE HIGHEST <br />
+            LEVEL PENTHOUSE
+          </h1>
+        </div>
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between z-10">
+          <a href="#slide1" className="btn btn-circle ml-20">
             ❮
           </a>
-          <a href="#slide3" className="btn btn-circle">
+          <a href="#slide3" className="btn btn-circle mr-20">
             ❯
           </a>
         </div>
       </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
+
+      {/* Slide 3 */}
+      <div id="slide3" className="carousel-item relative h-[900px] w-full">
+        <div className="absolute inset-0 z-0">
+          <img
+            src="https://i.ibb.co.com/HT695gDn/webaliser-TPTXZd9m-Oo-unsplash.jpg"
+            className="w-full h-full object-cover blur-sm"
+          />
         </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide3" className="btn btn-circle">
+        <div className="absolute top-1/3 left-20 text-white z-10">
+          <p className="bg-white bg-opacity-80 px-4 py-1 rounded text-black font-medium">
+            Miami,{" "}
+            <span className="text-red-600 font-semibold">South Florida</span>
+          </p>
+          <h1 className="text-5xl font-extrabold mt-4 leading-tight">
+            ACT NOW! <br />
+            GET THE HIGHEST <br />
+            LEVEL PENTHOUSE
+          </h1>
+        </div>
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between z-10">
+          <a href="#slide2" className="btn btn-circle ml-20">
             ❮
           </a>
-          <a href="#slide1" className="btn btn-circle">
+          <a href="#slide1" className="btn btn-circle mr-20">
             ❯
           </a>
         </div>
