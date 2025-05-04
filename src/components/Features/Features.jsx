64203af57060bdba2424fb1643cd08c4f0bf5ab9
Jayedjb765 <jayedjb76@gmail.com>
2025-05-04 23:55:05 +0600

    adding collapse

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import { Collapse } from "antd";
 import { RiCalendarScheduleLine } from "react-icons/ri";
 const Features = () => {
+  const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+  const items = [
+    {
+      key: "1",
+      label: "This is panel header 1",
+      children: <p>{text}</p>,
+    },
+    {
+      key: "2",
+      label: "This is panel header 2",
+      children: <p>{text}</p>,
+    },
+    {
+      key: "3",
+      label: "This is panel header 3",
+      children: <p>{text}</p>,
+    },
+  ];
   return (
     <div>
       <div className="flex">
@@ -18,6 +41,9 @@ const Features = () => {
             <h1 className="text-7xl font-semibold">
               Best Appartment <br /> & Sea view
             </h1>
+          </div>
+          <div>
+            <Collapse accordion items={items} />
           </div>
         </div>
       </div>
