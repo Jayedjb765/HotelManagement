7358c3d559b665cf27815cc6640228e749eb77db
Jayedjb765 <jayedjb76@gmail.com>
2025-05-04 23:52:35 +0600

    adding features for home page

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -3,13 +3,23 @@ import { RiCalendarScheduleLine } from "react-icons/ri";
 const Features = () => {
   return (
     <div>
-      <div className="ml-20">
-        <img
-          className="w-96 relative"
-          src="https://i.ibb.co.com/gLv1DGNs/aranprime-Kbyt-Cp-I1i5-I-unsplash.jpg"
-          alt=""
-        />
-        <RiCalendarScheduleLine className="text-9xl absolute -mt-16 -ml-20 rounded-full text-white bg-orange-500 p-4"></RiCalendarScheduleLine>
+      <div className="flex">
+        <div>
+          <img
+            className="w-[500px] relative"
+            src="https://i.ibb.co.com/gLv1DGNs/aranprime-Kbyt-Cp-I1i5-I-unsplash.jpg"
+            alt=""
+          />
+          <RiCalendarScheduleLine className="text-9xl absolute -mt-16 -ml-20 rounded-full text-white bg-orange-500 p-4"></RiCalendarScheduleLine>
+        </div>
+        <div>
+          <div>
+            <h3 className="text-2xl text-pink-400 font-bold">| Featured</h3>
+            <h1 className="text-7xl font-semibold">
+              Best Appartment <br /> & Sea view
+            </h1>
+          </div>
+        </div>
       </div>
     </div>
   );
