27d504be3f14643f99f18549ae33eba498c4b7a3
Jayedjb765 <jayedjb76@gmail.com>
2025-05-04 23:32:11 +0600

    adding features for home page

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { RiCalendarScheduleLine } from "react-icons/ri";
 const Features = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="ml-20">
+        <img
+          className="w-96 relative"
+          src="https://i.ibb.co.com/gLv1DGNs/aranprime-Kbyt-Cp-I1i5-I-unsplash.jpg"
+          alt=""
+        />
+        <RiCalendarScheduleLine className="text-9xl absolute -mt-16 -ml-20 rounded-full text-white bg-orange-500 p-4"></RiCalendarScheduleLine>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Slider from "../Slider/Slider";
+import Features from "../Features/Features";
 
 const Home = () => {
   return (
     <div>
       <Slider />
+      <Features />
     </div>
   );
 };
